18b91a0f8511f073ddab2dab48191e7cc8a95cf6
Franz <franz.zbinden7@gmail.com>
2025-10-10 13:22:15 -0400

    Refactor: Remove outdated operator examples and add comprehensive arithmetic, bitwise, comparison, logical, and membership operations

---
+++ 09_LeetCode-Problems/Eassy Problems/Hash Map/hashMapDemo.py
@@ -0,0 +1,11 @@
+# Generate a Dictionary / HashMap
+map = {}
+
+# Creates a set
+set = {"elem in set"}
+
+
+hashMap = {"key":"value", "key2":"val2", "key3": "val3"}
+
+print (type(hashMap))
+print(hashMap)
